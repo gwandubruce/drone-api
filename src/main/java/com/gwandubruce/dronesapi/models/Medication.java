75947ed7e414e4070667a32eff741eed23e29c6d
gwandubruce <gwandubruce@gmail.com>
2022-11-23 13:08:09 +0200

    added no argument and an all argument constructor

--- src/main/java/com/gwandubruce/dronesapi/models/Medication.java
+++ src/main/java/com/gwandubruce/dronesapi/models/Medication.java
@@ -1,16 +1,21 @@
 package com.gwandubruce.dronesapi.models;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
 @Entity
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Medication {
 
     @Id
     private Long id;
+
     private String name;
     private String weight;
     private String code;
