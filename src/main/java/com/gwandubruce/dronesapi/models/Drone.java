1d3e9bc73be833ead9d44f8b6b90877695a9730e
gwandubruce <gwandubruce@gmail.com>
2022-11-23 12:48:57 +0200

    Added member variables for the drone class

--- src/main/java/com/gwandubruce/dronesapi/models/Drone.java
+++ src/main/java/com/gwandubruce/dronesapi/models/Drone.java
@@ -1,10 +1,14 @@
 package com.gwandubruce.dronesapi.models;
 
+import com.gwandubruce.dronesapi.enumerations.Model;
+import com.gwandubruce.dronesapi.enumerations.State;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.Id;
 
 @Entity
@@ -15,5 +19,15 @@ public class Drone {
 
     @Id
     private Long id;
+
+    private String serialNumber;
+    private Integer weightLimit;
+    private Integer batteryCapacityPercent;
+
+    @Enumerated(EnumType.STRING)
+    private Model droneModel;
+    @Enumerated(EnumType.STRING)
+    private State state;
+
 
 }
