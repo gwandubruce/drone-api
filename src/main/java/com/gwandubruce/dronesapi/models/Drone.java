7c25ec39314b9338f42fafa2098d82c6df0468e7
gwandubruce <gwandubruce@gmail.com>
2022-11-23 14:33:49 +0200

    added one to many relationship between the Drone and the Medication

--- src/main/java/com/gwandubruce/dronesapi/models/Drone.java
+++ src/main/java/com/gwandubruce/dronesapi/models/Drone.java
@@ -6,10 +6,8 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.List;
 
 @Entity
 @Data
@@ -28,6 +26,9 @@ public class Drone {
     private Model droneModel;
     @Enumerated(EnumType.STRING)
     private State state;
+
+    @OneToMany
+    private List<Medication> medicationList;
 
 
 }
