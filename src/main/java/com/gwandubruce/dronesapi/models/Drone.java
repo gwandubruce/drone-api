4919f4de55c34d4975dbc37b241ca06cd05fd92b
gwandubruce <gwandubruce@gmail.com>
2022-11-23 11:59:11 +0200

    my first commit

---
+++ src/main/java/com/gwandubruce/dronesapi/models/Drone.java
@@ -0,0 +1,19 @@
+package com.gwandubruce.dronesapi.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Drone {
+
+    @Id
+    private Long id;
+
+}
