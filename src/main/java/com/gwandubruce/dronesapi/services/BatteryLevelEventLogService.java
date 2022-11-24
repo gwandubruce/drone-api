24342fc5ae157f377e98214ad17eb7ea4c5f5509
gwandubruce <gwandubruce@gmail.com>
2022-11-24 20:36:19 +0200

    added a scheduler method which monitors battery levels

--- src/main/java/com/gwandubruce/dronesapi/services/BatteryLevelEventLogService.java
+++ src/main/java/com/gwandubruce/dronesapi/services/BatteryLevelEventLogService.java
@@ -1,7 +1,41 @@
 package com.gwandubruce.dronesapi.services;
 
+import com.gwandubruce.dronesapi.models.BatteryLevelEventLog;
+import com.gwandubruce.dronesapi.models.Drone;
+import com.gwandubruce.dronesapi.repositories.BatteryLevelEventLogRepository;
+import com.gwandubruce.dronesapi.repositories.DroneRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
 @Service
+@AllArgsConstructor
 public class BatteryLevelEventLogService {
+
+    private final BatteryLevelEventLogRepository batteryLevelEventLogRepository;
+    private final DroneRepository droneRepository;
+
+    @Scheduled(fixedRate = 60_000)
+    public void monitorBatteryLevel() {
+
+        List<BatteryLevelEventLog> batteryLevelEventLog = new ArrayList<>();
+        Iterable<Drone> drones = droneRepository.findAll();
+
+        drones.forEach(drone -> batteryLevelEventLog.add(
+                BatteryLevelEventLog
+                        .builder()
+                        .logId(UUID.randomUUID().toString())
+                        .batteryLevel(drone.getBatteryCapacityPercent())
+                        .timeOfLogEntry(LocalDateTime.now())
+                        .drone(drone)
+                        .build()));
+
+        batteryLevelEventLogRepository.saveAll(batteryLevelEventLog);
+    }
 }
