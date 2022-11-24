44ecf6fc446d525681662b567fa179d9767b82ed
gwandubruce <gwandubruce@gmail.com>
2022-11-24 20:40:34 +0200

    added the @EnableScheduling annotagtion, intends to use a scheduler method in the app

--- src/main/java/com/gwandubruce/dronesapi/DronesApiApplication.java
+++ src/main/java/com/gwandubruce/dronesapi/DronesApiApplication.java
@@ -2,8 +2,10 @@ package com.gwandubruce.dronesapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class DronesApiApplication {
 
 	public static void main(String[] args) {
