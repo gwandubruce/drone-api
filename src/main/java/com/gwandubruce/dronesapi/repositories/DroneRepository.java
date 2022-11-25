47609fa374654a952ed01befdf5b054918b9e23c
gwandubruce <gwandubruce@gmail.com>
2022-11-26 00:00:35 +0200

    added custom native queries as well as custom methods for my DroneRepository

--- src/main/java/com/gwandubruce/dronesapi/repositories/DroneRepository.java
+++ src/main/java/com/gwandubruce/dronesapi/repositories/DroneRepository.java
@@ -1,7 +1,23 @@
 package com.gwandubruce.dronesapi.repositories;
 
+import com.gwandubruce.dronesapi.enumerations.State;
 import com.gwandubruce.dronesapi.models.Drone;
+import com.gwandubruce.dronesapi.models.Medication;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.Stack;
+
 public interface DroneRepository extends CrudRepository<Drone , String> {
+
+    Optional<Drone> findBySerialNumber(String serialNumber);
+    List<Drone> findByWeightLessThanAndBatteryGreaterThanAndStateIn(int weight, int batteryPercent, List<State> state);
+
+    @Query("select d.batteryCapacityPercent from Drone d where d.serialNumber = ?1")
+    Double findBatteryCapacityPercentBySerialNumber(String serialNumber);
+
+    @Query("select d.medicationList from Drone d where d.serialNumber = ?1")
+    List<Medication> findMedicationListBySerialNumber(String serialNumber);
 }
