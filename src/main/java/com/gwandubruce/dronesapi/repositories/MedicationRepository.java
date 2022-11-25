bc0629f6c89ca025c344531d7afa30b0aff51bf8
gwandubruce <gwandubruce@gmail.com>
2022-11-26 01:11:20 +0200

    added string parameter to findByCode method as well as defined a delete method

--- src/main/java/com/gwandubruce/dronesapi/repositories/MedicationRepository.java
+++ src/main/java/com/gwandubruce/dronesapi/repositories/MedicationRepository.java
@@ -8,6 +8,7 @@ import java.util.Optional;
 
 public interface MedicationRepository extends CrudRepository<Medication , String> {
 
-    Optional<Medication> findByCode ();
+    Optional<Medication> findByCode (String code);
+    boolean deleteByCode(String code);
     List<Medication> findByDroneId(String droneId);
 }
