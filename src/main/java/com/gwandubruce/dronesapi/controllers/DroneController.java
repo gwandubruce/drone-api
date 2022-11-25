47b1a7b9ebcfbaf2af2d41bae24c0cb2186aba79
gwandubruce <gwandubruce@gmail.com>
2022-11-26 01:33:41 +0200

    added end point for checking medication items of a drone using serial number

--- src/main/java/com/gwandubruce/dronesapi/controllers/DroneController.java
+++ src/main/java/com/gwandubruce/dronesapi/controllers/DroneController.java
@@ -1,11 +1,13 @@
 package com.gwandubruce.dronesapi.controllers;
 
 import com.gwandubruce.dronesapi.modelDTOs.DroneDTO;
+import com.gwandubruce.dronesapi.models.Medication;
 import com.gwandubruce.dronesapi.services.DroneService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/drone")
@@ -22,6 +24,13 @@ public class DroneController {
     public ResponseEntity<DroneDTO> register(@Valid @RequestBody DroneDTO drone) {
         DroneDTO response = droneService.registerDrone(drone);
         return ResponseEntity.ok(response);
+    }
+
+    @ResponseBody
+    @GetMapping(value = "/check/medication/{serialNumber}", produces = "application/json")
+    public ResponseEntity<List<Medication>> checkMedications(@PathVariable String serialNumber) {
+        List<Medication> medications = droneService.checkMedicationsOfADrone(serialNumber);
+        return ResponseEntity.ok(medications);
     }
 
 
