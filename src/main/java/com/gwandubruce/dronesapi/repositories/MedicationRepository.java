929919c2d2ab7ecbd59f72f76a2a54a52b0d8ef7
gwandubruce <gwandubruce@gmail.com>
2022-11-26 00:07:10 +0200

    added custom methods to my repo

--- src/main/java/com/gwandubruce/dronesapi/repositories/MedicationRepository.java
+++ src/main/java/com/gwandubruce/dronesapi/repositories/MedicationRepository.java
@@ -3,5 +3,11 @@ package com.gwandubruce.dronesapi.repositories;
 import com.gwandubruce.dronesapi.models.Medication;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface MedicationRepository extends CrudRepository<Medication , String> {
+
+    Optional<Medication> findByCode ();
+    List<Medication> findByDroneId(String droneId);
 }
