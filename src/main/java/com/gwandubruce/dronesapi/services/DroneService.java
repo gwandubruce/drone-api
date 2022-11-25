9c534b3f05de16fba701b02bde26e45a51ca2a2a
gwandubruce <gwandubruce@gmail.com>
2022-11-26 00:04:54 +0200

    1.Added three constants for use to retrieve loadable drones 2.changed the logic for my registerDrone method to include custom exceptions 3.Defined a method to retrieve all available drones and return to the client their respective serial numbers

--- src/main/java/com/gwandubruce/dronesapi/services/DroneService.java
+++ src/main/java/com/gwandubruce/dronesapi/services/DroneService.java
@@ -1,31 +1,66 @@
 package com.gwandubruce.dronesapi.services;
 
+import com.gwandubruce.dronesapi.enumerations.Model;
 import com.gwandubruce.dronesapi.enumerations.State;
+import com.gwandubruce.dronesapi.exceptions.DroneAlreadyExistsException;
+import com.gwandubruce.dronesapi.exceptions.DroneModelDoesNotExist;
 import com.gwandubruce.dronesapi.modelDTOs.DroneDTO;
 import com.gwandubruce.dronesapi.models.Drone;
 import com.gwandubruce.dronesapi.repositories.DroneRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.UUID;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @Service
 @AllArgsConstructor
 public class DroneService {
 
+    private static final int DRONE_MAX_WEIGHT_LIMIT = 500;
+    private static final int DRONE_MIN_BATTERY_LIMIT = 25;
+
+    private static final List<State> LOADABLE_STATES = Arrays.asList(State.IDLE,State.LOADING);
+
+
     private final DroneRepository droneRepository;
 
-    public DroneDTO registerDrone(DroneDTO drone) {
+    public DroneDTO registerDrone(DroneDTO droneDto) throws DroneAlreadyExistsException, DroneModelDoesNotExist {
 
-        Drone newDrone = new Drone(UUID.randomUUID().toString(), drone.getSerialNumber(), drone.getWeightLimit(),
-                drone.getBatteryCapacityPercent(), drone.getDroneModel(), State.IDLE, Collections.emptyList());
+        Optional<Drone> existingDrone = droneRepository.findBySerialNumber(droneDto.getSerialNumber());
+
+        if(existingDrone.isPresent()){
+
+            throw new DroneAlreadyExistsException();
+        }
+        if(isWrongModel(droneDto)){
+
+            throw new DroneModelDoesNotExist();
+        }
+
+        Drone newDrone = new Drone(UUID.randomUUID().toString(), droneDto.getSerialNumber(), droneDto.getWeightLimit(),
+                droneDto.getBatteryCapacityPercent(), droneDto.getDroneModel(), State.IDLE, Collections.emptyList());
 
         Drone savedDrone = droneRepository.save(newDrone);
 
         return new DroneDTO(savedDrone.getId(), savedDrone.getSerialNumber(), savedDrone.getWeightLimit(),
                 savedDrone.getBatteryCapacityPercent(), savedDrone.getDroneModel(), savedDrone.getState(),
                 savedDrone.getMedicationList());
+    }
+
+    public List<String> checkSerialNumbersOfDronesAvailable() {
+
+        List<Drone> drones = droneRepository.findByWeightLessThanAndBatteryGreaterThanAndStateIn(
+                DRONE_MAX_WEIGHT_LIMIT,
+                DRONE_MIN_BATTERY_LIMIT,
+                LOADABLE_STATES);
+
+      return   drones.stream()
+              .map(Drone::getSerialNumber)
+              .collect(Collectors.toList());
+    }
+
+    private boolean isWrongModel(DroneDTO drone){
+        return !(drone.getDroneModel() instanceof Model);
     }
 }
