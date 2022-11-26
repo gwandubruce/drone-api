debcb440d93a77471306cbf90c1f7574d168a08f
gwandubruce <gwandubruce@gmail.com>
2022-11-26 02:10:36 +0200

    exposed three endpoints for loading medications into drones, creating medications as well as deleting them

---
+++ src/main/java/com/gwandubruce/dronesapi/controllers/MedicationController.java
@@ -0,0 +1,42 @@
+package com.gwandubruce.dronesapi.controllers;
+
+import com.gwandubruce.dronesapi.modelDTOs.MedicationDTO;
+import com.gwandubruce.dronesapi.models.Medication;
+import com.gwandubruce.dronesapi.services.MedicationService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/api/medication")
+public class MedicationController{
+
+  private final MedicationService medicationService;
+
+  public MedicationController(MedicationService medicationService) {
+    this.medicationService = medicationService;
+  }
+
+  @ResponseBody
+  @PostMapping(value = "/load-medication", produces = "application/json", consumes = "application/json")
+  public ResponseEntity<Medication> loadMedication(@Valid @RequestBody MedicationDTO medicationDTO) {
+    Medication response = medicationService.loadMedicationIntoDrone(medicationDTO);
+    return ResponseEntity.ok(response);
+  }
+
+  @ResponseBody
+  @PostMapping(value = "/create")
+  public ResponseEntity<Medication> add(@Valid @RequestBody MedicationDTO medicationDto) {
+    Medication response = medicationService.addMedication(medicationDto);
+    return ResponseEntity.ok(response);
+  }
+
+  @ResponseBody
+  @DeleteMapping(value = "delete/{code}")
+  public ResponseEntity<Boolean> delete(@PathVariable("code") String code) {
+    Boolean response = medicationService.deleteMedicationByCode(code);
+    return ResponseEntity.ok(response);
+  }
+
+}
