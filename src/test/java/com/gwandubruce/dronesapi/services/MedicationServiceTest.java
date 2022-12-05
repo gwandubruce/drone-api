34ff889f01fb165c8200029b25c965301b189bff
gwandubruce <gwandubruce@gmail.com>
2022-12-05 16:12:45 +0200

    test method for loading drones (not implemented because of time)

---
+++ src/test/java/com/gwandubruce/dronesapi/services/MedicationServiceTest.java
@@ -0,0 +1,16 @@
+package com.gwandubruce.dronesapi.services;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class MedicationServiceTest {
+
+    @Before
+   public  void setUp() {
+    }
+
+    @Test
+   public void loadMedicationIntoDrone() {
+    }
+
+}
