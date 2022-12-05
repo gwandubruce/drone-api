2b827e453072302ccd590bbff4e8528913d1babc
gwandubruce <gwandubruce@gmail.com>
2022-12-05 13:02:49 +0200

    added the Builder annotation in order to use the builder pattern in the application for Drone objects

--- src/main/java/com/gwandubruce/dronesapi/models/Drone.java
+++ src/main/java/com/gwandubruce/dronesapi/models/Drone.java
@@ -3,6 +3,7 @@ package com.gwandubruce.dronesapi.models;
 import com.gwandubruce.dronesapi.enumerations.Model;
 import com.gwandubruce.dronesapi.enumerations.State;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -13,6 +14,7 @@ import java.util.List;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class Drone {
 
     @Id
