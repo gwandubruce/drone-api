4ff043b7fd0e27101b4472e651b9f4bc70e7d59d
gwandubruce <gwandubruce@gmail.com>
2022-12-05 15:44:16 +0200

    added an INVALID enum

--- src/main/java/com/gwandubruce/dronesapi/enumerations/Model.java
+++ src/main/java/com/gwandubruce/dronesapi/enumerations/Model.java
@@ -2,12 +2,25 @@ package com.gwandubruce.dronesapi.enumerations;
 
 public enum Model {
     LIGHT_WEIGHT("Light Weight"), MIDDLE_WEIGHT("Middle Weight"),
-    CRUISER_WEIGHT("Cruiser Weight"), HEAVY_WEIGHT("Heavy Weight");
+    CRUISER_WEIGHT("Cruiser Weight"), HEAVY_WEIGHT("Heavy Weight"), INVALID("Invalid Text");
 
     private String name;
 
     Model(String name) {
 
         this.name = name;
+    }
+
+    public static Model valueOfLabel (String name){
+
+        for(Model m : values()){
+
+            if(m.name.equalsIgnoreCase(name)){
+
+                return m;
+            }
+        }
+
+        return INVALID;
     }
 }
